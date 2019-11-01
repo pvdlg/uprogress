3f479cce8d25114b58e9cb758f4f0613e665998e
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2019-11-01 00:37:35 -0400

    build: update saucelabs browsers version

--- build/sauce-browsers.config.js
+++ build/sauce-browsers.config.js
@@ -11,7 +11,7 @@ module.exports = {
 	SL_Win_Chrome_Latest: {base: 'SauceLabs', browserName: 'chrome', platform: 'Windows 10', version: 'latest'},
 	SL_OSX_Safari_8: {base: 'SauceLabs', browserName: 'safari', platform: 'OS X 10.10', version: '8'},
 	SL_macOS_Safari_10: {base: 'SauceLabs', browserName: 'safari', platform: 'macOS 10.12', version: 'latest'},
-	SL_iOS_9: {base: 'SauceLabs', device: 'iPhone Simulator', browserName: 'iphone', platform: 'iOS', version: '9.3'},
+	SL_iOS_10: {base: 'SauceLabs', device: 'iPhone Simulator', browserName: 'iphone', platform: 'iOS', version: '10.3'},
 	SL_iOS_Latest: {
 		base: 'SauceLabs',
 		device: 'iPhone Simulator',
@@ -19,18 +19,19 @@ module.exports = {
 		platform: 'iOS',
 		version: 'latest',
 	},
-	SL_Android_4: {
+	SL_Android_5: {
 		base: 'SauceLabs',
 		device: 'Android Emulator',
 		browserName: 'android',
 		platform: 'android',
-		version: '4.4',
+		version: '5.1',
 	},
-	SL_Android_Latest: {
-		base: 'SauceLabs',
-		device: 'Android Emulator',
-		browserName: 'android',
-		platform: 'android',
-		version: 'latest',
-	},
+	// TODO re-enable when device is fixed on Saucelab
+	// SL_Android_Latest: {
+	// 	base: 'SauceLabs',
+	// 	device: 'Android Emulator',
+	// 	browserName: 'android',
+	// 	platform: 'android',
+	// 	version: 'latest',
+	// },
 };
